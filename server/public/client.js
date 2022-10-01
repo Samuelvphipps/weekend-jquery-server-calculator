06875b8ef7230a6436654ed59ff7aa9ad3bf7963
Sam Phipps <samuel.v.phipps@gmail.com>
2022-10-01 05:53:39 -0500

    get state and render to DOM

--- server/public/client.js
+++ server/public/client.js
@@ -2,15 +2,17 @@ $(document).ready(onReady);
 
 
 //STATE
-let calculations={};
+let calculationState=[];
+let calculationsLocal={};
 let operator='';
 
 function onReady(){
     console.log('in JQ and JS');
+    getState();
     //click listener for operator (*, /, +, -);
     $('.operator').on('click', onOperatorChoice);
     //click listener for submit
-    $('#equals').on('click', onSubmit)
+    $('#calculator-inputs').on('submit', onSubmit)
     //click listener for clear
     $('#clear').on('click', onClearCalculator)
 }
@@ -18,6 +20,7 @@ function onReady(){
 function onOperatorChoice(evt){
     evt.preventDefault();
     console.log('in onOperatorChoice');
+    //assign chosen operater to object property 'operator'
     calculations.operator= $(this).data('operator');
     console.log(calculations)
 }
@@ -35,16 +38,48 @@ function onClearCalculator(evt){
     console.log('in onClearCalculator')
 }
 
-function render(){
+function getState(){
+    console.log('in getState');
+    
+    // ajax GET
+    $.ajax({
+        url: '/mathInfo',
+        method: 'GET'
+    })
+        .then( (response)=> {
+            console.log('in /mathInfo get');
+            calculationState=response;
+            render();
+        })
+        .catch(err => {
+            console.log('in /mathInfo get error', err);
+        })
 
+}
+
+function render(){
+    console.log('in Render');
+    $('#historic-calculations').empty();
+    //for loop to append to DOM
+    for(let result of calculationState){
+        $('#historic-calculations').append(`
+        <li>${result.num1} ${result.operator} ${result.num2} = ${result.result} </li> 
+        `);
+    }   
+        //undefined, convert to number
+    // $('#result').empty();
+    // $('#result').append(`
+    //     <h2>${calculationState[calculationState.length -1]}.result
+    // `);
 }
 
 
 //stretch
 
+//regex?
+
+
 /*
 global variable, append each button and rerender
 on submit, post to server
-get to client
-
-*/
+get to client*/
